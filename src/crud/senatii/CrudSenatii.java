85e37a212f1aa9e1621c855c64f801acb67ae2ff
Jhordan <1557727@senati.pe>
2025-03-31 08:14:01 -0500

    Mi primero commit

---
+++ src/crud/senatii/CrudSenatii.java
@@ -0,0 +1,16 @@
+package crud.senatii;
+
+/**
+ *
+ * @author Alumno-ETI
+ */
+public class CrudSenatii {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
