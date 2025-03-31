a21f09c013561051eef54e821b0458eea03ba420
Jhordan <1557727@senati.pe>
2025-03-31 09:06:27 -0500

    Crear la clase conexion y su metodo conectar

---
+++ JavaApplication22/src/javaapplication22/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication22;
+
+import java.sql.*;
+/**
+ *
+ * @author Alumno-ETI
+ */
+public class Conexion {
+    public Connection conectar(){
+        String url = "jdbc:mysql://localhost:3308/senati?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        String user = "root";
+        String password = "1234";
+        Connection cn= null;
+        try{
+            cn = DriverManager.getConnection(url, user, password);
+            if (cn != null){
+                System.out.println("Conectado");
+            }
+        } catch (SQLException e){
+            System.out.println(e);
+        }
+        
+        return cn;
+    }
+    
+}
--- JavaApplication22/src/javaapplication22/JavaApplication22.java
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -14,7 +14,9 @@ public class JavaApplication22 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Conexion cn = new Conexion();
+        
+        cn.conectar();
     }
     
 }
